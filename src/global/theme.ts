8d5946f80ab39b9ed20781068c8257808e27b9e5
Ewerson Castelo Silva <ewerson.castelo@gmail.com>
2023-09-13 15:47:06 -0400

    chore: initial commit

---
+++ src/global/theme.ts
@@ -0,0 +1,19 @@
+export default {
+  colors: {
+    "primary-mid": "#8257E5",
+    "primary-light": "#996DFF",
+
+    background: "#121214",
+    "input-surface": "#202024",
+
+    "text-secondary": "#7C7C8A",
+    "text-primary": "#FFFFFF",
+
+    danger: "#E25858",
+  },
+
+  fonts: {
+    regular: "Inter_400Regular",
+    bold: "Inter_700Bold",
+  },
+}
